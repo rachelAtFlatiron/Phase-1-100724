6bfc2c9d6fdbbef5813ac805ad2c39cc5122b435
Rachel Katz <rachel.katz@flatironschool.com>
2024-06-18 15:39:35 -0400

    added review from matteo

--- src/index.js
+++ src/index.js
@@ -1,101 +1,76 @@
-//Data 
-const inventory = [
-  {
-    id: 1,
-    title: 'Eloquent JavaScript: A Modern Introduction to Programming',
-    author: 'Marjin Haverbeke',
-    price: 10.00,
-    reviews: [{userID: 1, content:'Good book, but not great for new coders'}],
-    inventory: 10,
-    imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg'
-  },
-  {
-    id: 2,
-    title: 'JavaScript & JQuery: Interactive Front-End Web Development',
-    author: 'Jon Duckett',
-    price: 45.75,
-    reviews: [{userID: 15, content:'good way to learn JQuery'}],
-    inventory: 2,
-    imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/31SRWF+LkKL._SX398_BO1,204,203,200_.jpg'
-  },
-  {
-    id: 3,
-    title: 'JavaScript: The Good Parts',
-    author: 'Douglas Crockford',
-    price: 36.00,
-    reviews: [{userID: 25, content:'I disagree with everything in this book'}, {userID: 250, content:'Only JS book anyone needs'}],
-    inventory: 8,
-    imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg'
-  },
-  {
-    id: 4,
-    title: 'JavaScript: The Definitive Guide',
-    author: 'David Flanagan',
-    price: 25.50,
-    reviews: [{userID: 44, content:'Great intro to js book'}, {userID: 350, content:'It really is the Definitive guide'}],
-    inventory: 0,
-    imageUrl: "https://images-na.ssl-images-amazon.com/images/I/51wijnc-Y8L._SX379_BO1,204,203,200_.jpg"
-  },
-  {
-    id: 5,
-    title: 'You Don’t Know JS',
-    author: 'Kyle Simpson',
-    price: 6.00,
-    reviews: [{userID: 76, content:'You can find this for free online, no need to pay for it!'}],
-    inventory: 7,
-    imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41T5H8u7fUL._SX331_BO1,204,203,200_.jpg'
-  }, 
-  {
-    id: 6,
-    title: 'Learn Enough JavaScript to Be Dangerous',
-    author: 'Michael Hartl',
-    price: 24.00,
-    reviews: [{userID: 50, content:'pretty good'}],
-    inventory: 5,
-    imageUrl: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQyf6xSyTHc7a8mx17ikh6GeTohc88Hn0UgkN-RNF-h4iOwVlkW'
-  },
-  {
-    id: 7,
-    title: 'Cracking the Coding Interview',
-    author: 'Gayle Laakmann McDowell',
-    price: 49.95,
-    reviews: [{userID: 99, content:'One of the most helpful books for taking on the tech interview'}, {userID: 20, content: 'Great but I just wish it was in JavaScript instead of Java' }],
-    inventory: 20,
-    imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SY344_BO1,204,203,200_.jpg'
-  }
-]
+'use strict' // use to enforce a stricter set of rules and best-practices
 
-//✅ Review variables (let, const)
-//✅ Review strings
-//✅ Review math
-//✅ Review if/else
-//✅ Review arrays?
-//✅ Review objects?
+// JS Fundamentals Part 1
 
-//✅ 1. Create hello world using REGULAR functions
-function helloWorld(){
-  return
+// Single Line Comments 
+
+/*
+    Multi Line Comments
+*/
+
+// Toggle comments using cmd + ? (mac) OR cntr + ? (pc)
+//! Special type of comment!
+
+// Console Logging
+// console.log(test)
+// console.error(test)
+// console.info(test)
+
+// Debugging
+// debugger
+
+// Variables
+var name = 'Matteo' // ES5, re-declaration: YES, re-assignment: YES, HOISTING!!!!
+let nameTwo = 'Matteo' //ES6 re-declaration (WITHIN SAME SCOPE): NO, re-assignment: YES, HOISTING???
+const nameThree = 'Matteo' //ES6 re-declaration (WITHIN SAME SCOPE): NO , re-assignment: NO, HOISTING???
+
+// Data Types
+
+// Boolean -> true, false
+//! Falsey values: undefined, null, "", false, 0, NaN, -0, 0n
+
+// String -> ', ", ` used only for string interpolation
+// Number -> integer, float/decimal, complex
+// Object -> array ([]), objects ({key: value})
+// Null -> absence of value
+// Undefined -> the value has not been assigned YET
+// BigInt -> +-(2^53 -1)
+// Symbol -> used for memory efficiency and in objects declarations
+
+
+// Conditional Statements
+// if (name == "Matteoh") {
+//     console.log("Yes")
+// } else if (name == "Matteog") {
+//     console.log("Yay")
+// } else {
+//     console.log("No")
+// }
+
+switch (name) {
+    case "Matteoh":
+        console.log("hello")
+        break
+    case "Marco":
+        console.log("World")
+        break
+    default:
+        console.log("Hello World!")
 }
-//✅ 2. For Easley's bookstore, create formatPrice(price)
 
-//✅ 3. Make an arrow function version of formatPrice
+// Ternary Expressions
+// expression ? returnValueIfTruthy : returnValueIfFalsey
+console.log(name.length > 5 ? 7 : 9)
+console.log("🚀 ~ file: index.js:61 ~ const age = name.length > 5 ? 7 : 9:")
 
-//✅ 4. WE DO: create a blurb() function that accepts a book as an argument and logs a message in the following format:
-//'Eloquent JavaScript: A Modern Introduction to Programming by Marjin Haverbeke is on sale for $10.00'
+// Loops
+for (let i = 0; i < 5; i++) {
+    console.log(i)
+}
 
-//✅ 5. Call formatPrice on an array of prices
+let counter = 0
 
-//✅ 5a. Create an array
-
-//✅ 5b. Use a for loop to iterate over prices
-
-//✅ 5c. Use .forEach to iterate over prices
-
-//✅ 5d. Use .map to iterate over prices
-
-//✅ 5e. using .map, for each book in inventory, return blurb(book)
-//output: ['Eloquent JavaScript: A Modern Introduction to Programming is on sale for $10.00', ...]
-
-//✅ 6. Create a version of myMap that uses a for loop to mimic .map
-//input: array, callback function
-//output: a new array
+while (counter < 5) {
+    console.log(counter)
+    counter++
+}
